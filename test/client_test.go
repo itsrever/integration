9faf482683b91780a1521a58688f3037437f40d0
Oscar <307765+osangenis@users.noreply.github.com>
2023-08-08 14:34:32 +0200

    making sure encoding works

--- test/client_test.go
+++ test/client_test.go
@@ -11,6 +11,10 @@ func TestApplyVars(t *testing.T) {
 	assert.Equal(t, applyVars("/orders/{orderId}", map[string]string{"other": "123"}), "/orders/{orderId}")
 	assert.Equal(t, applyVars("/orders/{orderId}{orderId}", map[string]string{"orderId": "123"}), "/orders/123123")
 	assert.Equal(t, applyVars("/orders/{orderId}/{modelId}", map[string]string{"orderId": "123", "modelId": "abc"}), "/orders/123/abc")
+	assert.Equal(t, applyVars("/orders/{orderId}", map[string]string{"orderId": "$&/"}), "/orders/%24%26%2F")
+	assert.Equal(t, applyVars("/orders?id={orderId}&sort=ASC", map[string]string{"orderId": "$&/"}), "/orders?id=%24%26%2F&sort=ASC")
+	assert.Equal(t, applyVars("/orders?id={orderId}&sort=ASC", map[string]string{"orderId": "123-345"}), "/orders?id=123-345&sort=ASC")
+	assert.Equal(t, applyVars("/orders?id={orderId}&sort=ASC", map[string]string{"orderId": "123/345"}), "/orders?id=123%2F345&sort=ASC")
 }
 
 func TestComposeURL(t *testing.T) {
