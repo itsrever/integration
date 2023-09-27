c9aa591f311299b2e4f628103fed441aec2dba75
bypepe77 <mario.espada77@gmail.com>
2023-09-27 16:02:16 +0200

    fix

--- test/add_note_test.go
+++ test/add_note_test.go
@@ -56,7 +56,7 @@ func Test_Add_Note_Into_Order(t *testing.T) {
 	})
 
 	t.Run("ADDNOTE04", func(t *testing.T) {
-		test.FailTestIfScenarioNotPresent(t, testName(t))
+		test.SkipTestIfScenarioNotPresent(t, testName(t))
 		scenario := test.Scenario(testName(t))
 		resp, err := c.Do("POST", test.UrlPattern, scenario.Vars(), noteBody)
 		require.NoError(t, err)
